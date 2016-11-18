cfc91b9a8169ec8b580ed66dcc4b2bb72e8eeee0
Basim Hennawi <basim.a.hennawi@gmail.com>
2016-11-18 02:44:15 +0200

    Add newrelic.js with license key

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Basim Hennawi'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '78ee3cf235bfa180fd7a67194589ffdcb57495f5',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
