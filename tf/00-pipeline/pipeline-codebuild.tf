791e3c39a6f7886a78c634e17bc692b053501de5
Basim Hennawi <basim.a.hennawi@gmail.com>
2021-07-01 13:34:50 +0200

    hotfix path

--- tf/00-pipeline/pipeline-codebuild.tf
+++ tf/00-pipeline/pipeline-codebuild.tf
@@ -4,7 +4,7 @@ module "codebuild_deploy" {
   project_name        = "${var.app}-${var.service}-deploy"
   role_arn            = aws_iam_role.codebuild_role.arn
   buildspec           = "tf/00-pipeline/buildspecs/deploy.yaml"
-  target_directory    = "10-infrastructure"
+  target_directory    = "tf/10-infrastructure"
   env                 = var.env
   vpc_id              = data.aws_vpc.pipeline.id
   subnet_ids          = data.aws_subnet_ids.pipeline_private.ids
