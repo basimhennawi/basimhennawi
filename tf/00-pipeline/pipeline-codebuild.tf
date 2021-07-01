bc4046517edf5c8c340d00985fee417c59aad893
Basim Hennawi <basim.a.hennawi@gmail.com>
2021-07-01 13:29:40 +0200

    upgrade tf version

--- tf/00-pipeline/pipeline-codebuild.tf
+++ tf/00-pipeline/pipeline-codebuild.tf
@@ -3,7 +3,7 @@ module "codebuild_deploy" {
 
   project_name        = "${var.app}-${var.service}-deploy"
   role_arn            = aws_iam_role.codebuild_role.arn
-  buildspec           = "00-pipeline/buildspecs/deploy.yaml"
+  buildspec           = "tf/00-pipeline/buildspecs/deploy.yaml"
   target_directory    = "10-infrastructure"
   env                 = var.env
   vpc_id              = data.aws_vpc.pipeline.id
